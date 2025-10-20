11783f8ec89a5c1b0cb69667e463ecb5e88c2e01
shelepuginivan <shelepuginivanm@gmail.com>
2025-10-20 07:19:47 +0300

    build(website): update info

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,26 +1,38 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config"
+import starlight from "@astrojs/starlight"
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
-});
+    i18n: {
+        locales: ["ru"],
+        defaultLocale: "ru",
+    },
+
+    integrations: [
+        starlight({
+            defaultLocale: "ru",
+            title: { ru: "Hestia", en: "Hestia" },
+            social: [
+                {
+                    icon: "github",
+                    label: "GitHub",
+                    href: "https://github.com/moevm4388/hestia",
+                },
+            ],
+            sidebar: [
+                {
+                    label: "Guides",
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: "Example Guide", slug: "guides/example" },
+                    ],
+                },
+                {
+                    label: "Reference",
+                    autogenerate: { directory: "reference" },
+                },
+            ],
+        }),
+    ],
+})
