8af662893af7eef01ed4701ed2f21ac2b0d640d2
shelepuginivan <shelepuginivanm@gmail.com>
2025-10-20 06:41:30 +0300

    content(website): add comments content collection

--- website/src/content.config.ts
+++ website/src/content.config.ts
@@ -1,7 +1,22 @@
 import { defineCollection } from 'astro:content';
+import { file } from 'astro/loaders';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
+import { z } from 'astro:schema'
 
-export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-};
+
+const comments = defineCollection({
+    loader: file("./src/content/comments/comments.yaml"),
+    schema: z.object({
+        id: z.number().positive(),
+        name: z.string(),
+        login: z.string(),
+        body: z.string(),
+        avatar: z.string().url(),
+        href: z.string().url(),
+    })
+})
+
+const docs = defineCollection({ loader: docsLoader(), schema: docsSchema() })
+
+export const collections = { comments, docs };
