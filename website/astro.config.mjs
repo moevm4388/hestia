259439bf114357338f36729d2c6e5e95820afbd3
shelepuginivan <shelepuginivanm@gmail.com>
2025-11-11 05:16:35 +0300

    fix(website): set base

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -4,7 +4,8 @@ import starlight from "@astrojs/starlight"
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://moevm4388.github.io/hestia',
+    site: "https://moevm4388.github.io",
+    base: "/hestia",
 
     i18n: {
         locales: ["ru"],
